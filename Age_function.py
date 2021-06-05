39afe880ec7a61fb4f12c9ab3d525c3f6d7c7949
Rohanlikescoding <47610596+Rohanlikescoding@users.noreply.github.com>
2021-06-05 16:58:14 -0400

    Add files via upload
    the function of ages has three age variables and outside the age function the user inputs his own age and 2 other ages of people the user knows about and those numbers get stored into the function variables to add up all the ages.

---
+++ Age_function.py
@@ -0,0 +1,13 @@
+def ages(age1,age2,age3):
+    sol = age1 + age2 + age3
+    return sol
+one = int(input("enter your age? "))
+two = int(input("enter other persons age? "))
+three = int(input("enter another persons age? "))
+values = ages(one,two,three)
+print("Calculation of all ages is " + str(values))
+
+
+
+
+
